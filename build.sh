65f5ca1ec2d576e24549e604d09cf43e32a3f2e0
progfay <shun1856shun@gmail.com>
2020-04-06 00:17:07 +0900

    fix: remove .alfredworkflow before build

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-app_name=Gyalfred
 exclude=''
-
-while read pattern
-do
-	exclude="${exclude} ${pattern/\*/\*}"
-done < .alfredignore
-
-sh -c "zip -r ${app_name}.alfredworkflow . -x ${exclude}"
-
+while read pattern; do exclude="${exclude} ${pattern/\*/\*}"; done < .alfredignore
+sh -c "rm Gyalfred.alfredworkflow; zip -r Gyalfred.alfredworkflow . -x ${exclude}"
